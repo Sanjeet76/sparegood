0556b7baa5d66157c59974f75dc14f99c5cda70e
sanjeet kumar <3676sanjeetkumargupta@gmail.com>
2024-02-28 13:34:38 +0530

    file added 404.html

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import Home from './components/Home/Home';
 function App() {
   return (
     
-    <Router>
+    <Router basename='/tothepoint_login'>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} /> 
